d0f4c96cb13e382063ca9d54b98aad3e14bbbc91
Ed Guinto <eguinto@rdxsupport.com>
2024-11-06 22:06:54 +0800

    update config

--- smf-xendit-fe/services/config/config.js
+++ smf-xendit-fe/services/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
     // apiUrl: "http://localhost:3000",
-    apiUrl: "https://payments-api.serye.fm:3000",
+    apiUrl: "https://payments-api.serye.fm",
     successReturnUrl: "https://www.serye.fm/payment-confirmation",
     failureReturnUrl: "https://www.serye.fm/payment-failure",
 };
